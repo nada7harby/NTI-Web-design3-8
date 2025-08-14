3b0c9176063e335bfeae44b726dfed283e6bd5a8
nada7harby <87335429+nada7harby@users.noreply.github.com>
2025-08-14 13:47:05 +0300

    day 10

---
+++ day 10/script.js
@@ -0,0 +1,102 @@
+var name = "nadssssssa";
+
+var arr = [1, 5, 6, true, "Nour"]; //index
+
+let obj1 = {
+  name: "aliiiii",
+  age: 99,
+  active: true,
+};
+
+console.log(arr[3]);
+console.log(obj1.name);
+
+// alert("Welcome in our website")
+
+var x = 20;
+var y = 15;
+console.log(y % x);
+
+console.log(x != y);
+console.log(x == y);
+console.log(x === y);
+
+// and
+console.log(x > 5 && x == y);
+console.log(x > 5 && x < y);
+
+// OR
+console.log(x > 5 || x == y);
+console.log(x < 5 || x == y);
+
+// not
+console.log(!(x == y));
+console.log(!(x < 5 || x == y));
+
+if (x < y) {
+  console.log("X is smaller");
+  alert("X is smaller");
+} else if (x > y) {
+  console.log(" X is bigger");
+} else if (x != y) {
+  console.log(" X == Y");
+} else {
+  console.log("noooo");
+}
+
+console.log("hi");
+
+// var i = 2;
+// console.log(i++); //2
+// console.log(++i);//4
+
+for (let i = 0; i < 10; i++) {
+  console.log("hi"); //0 1 2 3   ..10
+}
+
+var i = 2;
+while (i < 10) {
+  console.log("hello while");
+  i += 2;
+}
+
+x = 3;
+console.log(x);
+
+// var z = 2;
+// do {
+//   console.log(" hello DO");
+//   z++;
+// } while (z > 10);
+
+// var name1 = prompt();
+// console.log(name1);
+
+// var agee = prompt("enter you age");
+
+// if (agee > 18) {
+//   alert("you are adult");
+// }
+
+
+var name2=prompt()
+
+console.log("hello " + name2);
+
+
+
+function send() {
+if (x < y) {
+  console.log("X is smaller");
+  alert("X is smaller");
+} else if (x > y) {
+  console.log(" X is bigger");
+} else if (x != y) {
+  console.log(" X == Y");
+} else {
+  console.log("noooo");
+}
+
+}
+
+
